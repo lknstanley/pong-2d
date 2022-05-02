665da71133a3a45a4b946d4f4aa9aa6500050a36
Stanley Lai <stanley.lai.104@gmail.com>
2022-05-02 14:01:29 +0800

    Added level serialized class

---
+++ Assets/Scripts/Models/LevelData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Models
+{
+    [ Serializable ]
+    public class LevelData
+    {
+        public int level;
+        public int mapWidth;
+        public int mapHeight;
+        public int[] map;
+    }
+}
