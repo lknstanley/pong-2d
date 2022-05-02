00f47fe5373a3cc76002688897087eb7e317a9bd
Stanley Lai <stanley.lai.104@gmail.com>
2022-05-02 20:15:28 +0800

    Added main menu scene

---
+++ Assets/Scripts/UI/MainMenuScreen/UIMainMenuScreen.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI.MainMenuScreen
+{
+    public class UIMainMenuScreen : MonoBehaviour
+    {
+        #region Event Handlers
+
+        public void OnStartGameClicked()
+        {
+            SceneManager.LoadScene( "MainGameScene" );
+        }
+
+        public void OnExitGameClicked()
+        {
+            Application.Quit();
+        }
+
+        #endregion
+    }
+}
