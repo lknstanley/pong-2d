0959c68fb2acb2db5d5aed08bd0407c9caa65c2d
Stanley Lai <stanley.lai.104@gmail.com>
2022-05-02 14:03:18 +0800

    Added brick class to broadcast event

---
+++ Assets/Scripts/Gameplay/Brick.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Core;
+using UnityEngine;
+
+namespace Gameplay
+{
+    public class Brick : MonoBehaviour
+    {
+        private void OnCollisionEnter2D( Collision2D collision )
+        {
+            // Trigger destroy brick event
+            PongGameManager.GetInstance().GetEventManager()
+                           .BroadcastEvent( PongEventManager.PongEventType.DestroyBrick );
+            
+            // Destroy self
+            Destroy( gameObject );
+        }
+    }
+}
