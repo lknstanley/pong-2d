f342a1b89841a800ea0da58a90fe1adee8f81252
Stanley Lai <stanley.lai.104@gmail.com>
2022-05-02 14:03:02 +0800

    Added laser wall to deduct player health when the ball hit it

---
+++ Assets/Scripts/Gameplay/LaserWall.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Core;
+using UnityEngine;
+
+namespace Gameplay
+{
+    public class LaserWall : MonoBehaviour
+    {
+        private void OnCollisionEnter2D( Collision2D collision )
+        {
+            PongGameManager.GetInstance().GetEventManager().BroadcastEvent( PongEventManager.PongEventType.LoseHealth );
+        }
+    }
+}
