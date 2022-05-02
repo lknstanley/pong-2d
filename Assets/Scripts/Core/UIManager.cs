ae107fc9067e55181d7f09a95bc55daf663cc23b
Stanley Lai <stanley.lai.104@gmail.com>
2022-05-02 14:02:43 +0800

    Added UI manager and references to each screen

---
+++ Assets/Scripts/Core/UIManager.cs
@@ -0,0 +1,10 @@
+using UI.MainGameScreen;
+using UnityEngine;
+
+namespace Core
+{
+    public class UIManager : MonoBehaviour
+    {
+        public UIMainGameScreen mainGameScreen;
+    }
+}
---
+++ Assets/Scripts/UI/MainGameScreen/UIMainGameScreen.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace UI.MainGameScreen
+{
+    public class UIMainGameScreen : MonoBehaviour
+    {
+        [ Header( "UI References" ) ]
+        public RectTransform heartsContainer;
+    }
+}
