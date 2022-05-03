3e8226022c4078f6201bc5c297a9456645bfd68b
Stanley Lai <stanley@success-base.com>
2022-05-03 19:40:11 +0800

    Created player status model

---
+++ Assets/Scripts/Models/PlayerStatus.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Models
+{
+    [ Serializable ]
+    public struct PlayerStatus
+    {
+        public int maxHealth;
+        public int currentHealth;
+        public int currentLevel;
+    }
+}
